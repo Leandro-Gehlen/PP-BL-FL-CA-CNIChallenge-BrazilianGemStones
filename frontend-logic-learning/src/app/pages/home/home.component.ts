74d9f2b29bc900f22a0a2e609a04636769b39a25
Leandro Gehlen <legedeveloper@gmail.com>
2023-03-08 08:14:28 -0300

    creating some exercises about ngFor, property bindind and routing on home page

--- frontend-logic-learning/src/app/pages/home/home.component.ts
+++ frontend-logic-learning/src/app/pages/home/home.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent {
+
+  courseURL: string = "https://www.youtube.com/watch?v=CGLdH5ORX-Y&t=4979s"
+
+  subjectsList: Array<any> = [
+    { id: 1, subjectTitle: "How to exchange data between components" }
+  ]
 
 }
